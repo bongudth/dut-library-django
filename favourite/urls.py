4e62d423e4fa8279c1325822022cda6f2321b352
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-06-15 13:53:18 +0700

    :zap: build the favourite page

--- favourite/urls.py
+++ favourite/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'favourite'
+
 urlpatterns = [
     path('', views.book_favourite, name='book_favourite'),
 ]
--- favourite/views.py
+++ favourite/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def book_favourite(request):
+    return render(request, 'library/favourite/favourite.html')
